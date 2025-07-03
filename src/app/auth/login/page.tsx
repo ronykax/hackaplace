9963c8042b48af479ac22aeb9eb126349d664d11
Rony Kati <73883742+ronykax@users.noreply.github.com>
2025-07-03 23:55:12 +0530

    fix origin url

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
                 provider: "slack_oidc",
                 options: {
                     scopes: "openid email profile",
-                    redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
+                    redirectTo: `${window.location.origin}/auth/callback`,
                 },
             });
         })();
